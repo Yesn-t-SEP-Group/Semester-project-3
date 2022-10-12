1d8b9592ce37e59acbefe8ea6f7e4ed9733fdf59
Raedrim <43709947+Raedrim@users.noreply.github.com>
2022-10-12 10:53:13 +0200

    Created projects for tier 1 and tier 2

---
+++ SEP_T2/src/main/java/via/dk/sep_t2/SepT2Application.java
@@ -0,0 +1,15 @@
+package via.dk.sep_t2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepT2Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SepT2Application.class, args);
+    }
+
+}
---
+++ SEP_T2/src/test/java/via/dk/sep_t2/SepT2ApplicationTests.java
@@ -0,0 +1,15 @@
+package via.dk.sep_t2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepT2ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
