629799338c3648473e317dbab22e91987200b80f
MartyBobo <martin.rosendahl4@gmail.com>
2022-10-28 10:03:25 +0200

    blazor basic website  with authentication and pages

---
+++ SEP_T1/Shared/Models/User.cs
@@ -0,0 +1,36 @@
+﻿namespace Shared.Models;
+
+public class User
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public string Address { get; set; }
+
+    public Boolean isAdmin { get; set; }
+
+    public User(string username, string password, bool isAdmin)
+    {
+        Username = username;
+        Password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public User(string username, string password, string name, string email, string phoneNumber, string address, bool isAdmin)
+    {
+        Username = username;
+        Password = password;
+        Name = name;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        Address = address;
+        this.isAdmin = isAdmin;
+    }
+
+    public User()
+    {
+    }
+}
---
+++ SEP_T1/WebApi/Services/AuthService.cs
@@ -0,0 +1,86 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Shared.Models;
+
+namespace WebApi.Services;
+
+public class AuthService : IAuthService
+{
+
+    private readonly IList<User> users = new List<User>
+    {/*
+        new User
+        {
+            Email = "trmo@via.dk",
+            Name = "Troels Mortensen",
+            Password = "onetwo3FOUR",
+            Username = "trmo",
+        },
+        new User
+        {
+           
+            Email = "jakob@gmail.com",
+          
+            Name = "Jakob Rasmussen",
+            Password = "password",
+    
+            Username = "jknr",
+         
+        }
+        */
+    };
+    
+
+    public Task<User> ValidateUser(string username, string password)
+    {
+        User? existingUser = users.FirstOrDefault(u => u.Username.Equals(username, StringComparison.OrdinalIgnoreCase));
+        if (existingUser == null)
+        {
+            throw new Exception("User not found");
+        }
+
+        if (!existingUser.Password.Equals(password))
+        {
+            throw new Exception("Password mismatch");
+        }
+
+        return Task.FromResult(existingUser);
+    }
+
+    public Task RegisterUser(User user)
+    {
+
+        if (string.IsNullOrEmpty(user.Username))
+        {
+            throw new ValidationException("Username cannot be null");
+        }
+
+        if (string.IsNullOrEmpty(user.Password))
+        {
+            throw new ValidationException("Password cannot be null");
+        }
+        
+        if (string.IsNullOrEmpty(user.Name))
+        {
+            throw new ValidationException("Name cannot be null");
+        }
+        if (string.IsNullOrEmpty(user.Email))
+        {
+            throw new ValidationException("Email cannot be null");
+        }
+        if (string.IsNullOrEmpty(user.PhoneNumber))
+        {
+            throw new ValidationException("PhoneNumber cannot be null");
+        }
+        if (string.IsNullOrEmpty(user.Address))
+        {
+            throw new ValidationException("Address cannot be null");
+        }
+        // Do more user info validation here
+        
+        // save to persistence instead of list
+        
+        users.Add(user);
+        
+        return Task.CompletedTask;
+    }
+}
