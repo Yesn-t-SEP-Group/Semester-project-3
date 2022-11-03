9d1de650e59fded6c2485346926b21c6621e69b9
KrunoNeric <91593987+KrunoNeric@users.noreply.github.com>
2022-11-03 10:13:37 +0100

    Revert "Set up model, repository for T3"
    This reverts commit dd949ab95f6c8d49514761226e3e84c29c27234b.
    
    # Conflicts:
    #	SEP_T3/pom.xml

--- SEP_T3/src/main/java/via/sdj3/sep_t3/repository/ItemRegistry.java
+++
@@ -1,9 +0,0 @@
-package via.sdj3.sep_t3.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import via.sdj3.sep_t3.model.Item;
-
-public interface ItemRegistry extends CrudRepository<Item,Integer>
-{
-    //write sql specific methods here
-}
--- SEP_T3/src/main/java/via/sdj3/sep_t3/repository/UserRegistry.java
+++
@@ -1,8 +0,0 @@
-package via.sdj3.sep_t3.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import via.sdj3.sep_t3.model.User;
-
-public interface UserRegistry extends CrudRepository<User,Integer>
-{
-}
