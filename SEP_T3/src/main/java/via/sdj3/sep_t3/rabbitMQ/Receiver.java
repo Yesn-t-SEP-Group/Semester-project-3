acb3ac7dc403e46f8373f73b3d0c2352750eb847
Raedrim <43709947+Raedrim@users.noreply.github.com>
2022-11-07 15:53:35 +0100

    Putting user in database from values sent from backend
    YS-76

--- SEP_T3/src/main/java/via/sdj3/sep_t3/rabbitMQ/Receiver.java
+++ SEP_T3/src/main/java/via/sdj3/sep_t3/rabbitMQ/Receiver.java
@@ -1,44 +1,79 @@
 package via.sdj3.sep_t3.rabbitMQ;
 
-import java.io.UnsupportedEncodingException;
-import java.util.concurrent.CountDownLatch;
-
+import com.google.gson.Gson;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import via.sdj3.sep_t3.model.Post;
+import via.sdj3.sep_t3.backendModel.BackendUser;
 import via.sdj3.sep_t3.model.User;
 import via.sdj3.sep_t3.repository.PostRegistry;
 import via.sdj3.sep_t3.repository.UserRegistry;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+
 @Component
-public class Receiver {
+public class Receiver
+{
     @Autowired
     private UserRegistry userRegistry;
 
     @Autowired
     private PostRegistry postRegistry;
+
     @RabbitHandler
-    public void receiveMessage(byte[] body) {
-        String message= null;
+    public void receiveMessage(byte[] body)
+    {
+        String message = null;
         try
         {
-            message = new String(body,"UTF-8");
-            for(User user:userRegistry.findAll())
+            message = new String(body, "UTF-8");
+            Gson gson = new Gson();
+
+            String[] messageSplit = message.split(";");
+
+            switch (messageSplit[0])
+            {
+                case"createNewUser":
+                {
+                    BackendUser fromJson = gson.fromJson(messageSplit[1], BackendUser.class);
+                    User user =new User();
+
+                    user.setUsername(fromJson.getUsername());
+                    user.setUserpass(fromJson.getPassword());
+                    user.setAddress(fromJson.getAddress());
+                    user.setFullName(fromJson.getFullName());
+                    user.setEmail("test");
+                    user.setPhoneNumber(fromJson.getPhoneNo());
+                    user.setAddress(fromJson.getAddress());
+                    user.setRating(BigDecimal.valueOf(fromJson.getRating()));
+
+                    userRegistry.save(user);
+
+                    break;
+                }
+            }
+            /*
+            for (User user : userRegistry.findAll())
             {
                 System.out.println(user.getFullName());
             }
             postRegistry.findAll().forEach(post -> System.out.println(post.getDescription()));
-            for(User user:userRegistry.findByAddress("Horsens"))
+            for (User user : userRegistry.findByAddress("Horsens"))
             {
                 System.out.println(user.getFullName());
             }
+            User test = new User();
+
+            userRegistry.save(test);
+             */
+
 
         } catch (UnsupportedEncodingException e)
         {
             throw new RuntimeException(e);
         }
-        System.out.println("Received <" + message+ ">");
+        System.out.println("Received <" + message + ">");
 
     }
 }
