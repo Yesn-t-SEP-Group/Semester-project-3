6f4353370ef1e5d2a5f3a2ff692ff6e431979090
Raedrim <43709947+Raedrim@users.noreply.github.com>
2022-11-10 18:44:30 +0100

    no message

---
+++ SEP_T1/Domain/Models/Error.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    using System.Text.Json;
+
+    public class Error
+    {
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
