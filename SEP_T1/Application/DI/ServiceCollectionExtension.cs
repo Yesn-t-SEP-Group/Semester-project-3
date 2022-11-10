105085f0ce6d234b7cdf212c97e98f7f154226a8
Raedrim <43709947+Raedrim@users.noreply.github.com>
2022-11-10 18:33:31 +0100

    webapi startup class rewrite, dependency injection implementations, added serilog logging to console and in file

---
+++ SEP_T1/Application/DI/ServiceCollectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using Application.DaoInterfaces;
+using Application.Logic;
+using Application.LogicInterfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DI
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddBusinessLogic(this IServiceCollection services)
+        {
+            services.AddScoped<IUserLogic, UserLogic>();
+            services.AddScoped<IPostLogic, PostLogic>();
+            return services;
+        }
+    }
+}
---
+++ SEP_T1/FileData/DI/ServiceCollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using Application.DaoInterfaces;
+using Application.Logic;
+using Application.LogicInterfaces;
+using FileData.DAOs;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileData.DI
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddFileDaos(this IServiceCollection services)
+        {
+            services.AddScoped<FileContext>();
+            services.AddScoped<IUserDao, UserFileDao>();
+            services.AddScoped<IPostDao, PostFileDao>();
+            return services;
+        }
+    }
+}
