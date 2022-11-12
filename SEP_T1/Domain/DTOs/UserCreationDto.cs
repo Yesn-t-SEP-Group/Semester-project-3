a750f144d64e606b8f5f853309ad467d15ac1f07
gabrielvlad <gabriel.vlad@outlook.dk>
2022-11-12 19:34:06 +0100

    frontend for create user looks correctly
    changed how the UI looks for the create user so labels are now inside the input fields

--- SEP_T1/Domain/DTOs/UserCreationDto.cs
+++ SEP_T1/Domain/DTOs/UserCreationDto.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Runtime.CompilerServices;
 
 namespace Domain.DTOs;
 
@@ -7,35 +6,35 @@ public class UserCreationDto
 {
     [Required(ErrorMessage = "Username is required")]
     [StringLength(16, ErrorMessage = "Must be between 3 and 16 characters", MinimumLength = 3)]
-    public string UserName { get; set; }
+    public string UserName { get; set; } = String.Empty;
     
     [Required(ErrorMessage = "Password is required")]
     [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
     [DataType(DataType.Password)]
-    public string Password { get; set; }
+    public string Password { get; set; } = String.Empty;
 
     [Required(ErrorMessage = "Confirm Password is required")]
     [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
     [DataType(DataType.Password)]
     [Compare("Password")]
-    public string ConfirmPassword { get; set; }
+    public string ConfirmPassword { get; set; } = String.Empty;
     
     [Required(ErrorMessage = "Must select a role")]
-    public string Role { get; set; }
+    public string Role { get; set; } = String.Empty;
     
     [Required(ErrorMessage = "Enter full name")]
-    public string Name { get; set; }
+    public string Name { get; set; } = String.Empty;
     [Required]
     [EmailAddress(ErrorMessage = "Enter valid email address")] 
-    public string Email { get; set; }
+    public string Email { get; set; } = String.Empty;
     
     [Required(ErrorMessage = "You must provide a phone number")]
     [Display(Name = "Mobile Phone")]
     [DataType(DataType.PhoneNumber)]
     [RegularExpression(@"^(\+|00)45\d{8}", ErrorMessage = "The phone number must be from Denmark")]
-    public string PhoneNumber { get; set; }
+    public string PhoneNumber { get; set; } = String.Empty;
     
     [Required(ErrorMessage = "You must provide an address")]
     [StringLength(200, MinimumLength = 5)]
-    public string Address { get; set; }
+    public string Address { get; set; } = String.Empty;
 }
