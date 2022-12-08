e80e7b403a9f1bb5977511d2a3b12a67c832fade
MartyBobo <martin.rosendahl4@gmail.com>
2022-12-08 11:54:24 +0100

    added post id and post title to messsages

--- SEP_T1/Domain/DTOs/MessageCreationDto.cs
+++ SEP_T1/Domain/DTOs/MessageCreationDto.cs
@@ -5,4 +5,6 @@ public class MessageCreationDto
     public int UserFromId { get; set; }
     public int UserToId { get; set; }
     public string MessageText { get; set; }
+    
+    public int postId { get; set; }
 }
--- SEP_T1/Domain/DTOs/MessageReadDto.cs
+++ SEP_T1/Domain/DTOs/MessageReadDto.cs
@@ -7,5 +7,9 @@ public class MessageReadDto
     public int UserToId { get; set; }
     public string MessageText { get; set; }
     public string? UserFromName { get; set; }
+    
+    public int postId { get; set; }
+    
+    public string? postTitle { get; set; }
 
 }
